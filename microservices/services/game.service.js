96ff094e0b64e1ae0df7e3e9f3dd205e7eee8693
nairod49 <78696310+nairod49@users.noreply.github.com>
2021-11-26 13:48:29 +0100

    service categorie et game

---
+++ microservices/services/game.service.js
@@ -0,0 +1,63 @@
+"use strict";
+
+const DbMixin = require("../mixins/db.mixin");
+
+/**
+ * @typedef {import('moleculer').Context} Context Moleculer's Context
+ */
+
+module.exports = {
+	name: "game",
+	// version: 1
+
+	/**
+	 * Mixins
+	 */
+	mixins: [DbMixin("game")],
+
+	/**
+	 * Settings
+	 */
+	settings: {
+		// Available fields in the responses
+		fields: [
+			"_id",
+			"date",
+            "score",
+			"id_categorie",
+            "id_user",
+            "difficulté"
+		],
+
+		
+		
+	},
+
+	/**
+	 * Action Hooks
+	 */
+	hooks: {
+	},
+
+	/**
+	 * Actions
+	 */
+	actions: {
+
+
+        
+
+
+    },
+
+	/**
+	 * Methods
+	 */
+	methods: {
+	},
+
+	/**
+	 * Fired after database connection establishing.
+	 */
+	
+};
