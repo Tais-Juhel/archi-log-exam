81f86a3605f74aef3233e4c338da93f11e2f7cc7
Tais-Juhel <45936782+Tais-Juhel@users.noreply.github.com>
2021-12-01 21:32:12 +0100

    vue design update

--- mvc/src/components/ButtonCustom.vue
+++ mvc/src/components/ButtonCustom.vue
@@ -21,16 +21,38 @@ export default {
   display: flex;
   justify-content: center;
   align-items: center;
-  padding: 0 40px;
 
+  //size
+  &.small {
+    height: 50px;
+    padding: 0 80px;
+    border-radius: 25px;
+    font-size: 24px;
+  }
   &.big {
     height: 80px;
     border-radius: 40px;
     font-size: 36px;
+    padding: 0 40px;
   }
 
+  //color
+  &.light {
+    background-color: #FEFCFB;
+
+    a {
+      color: #034078;
+    }
+  }
   &.dark {
     background-color: #034078;
+
+    a {
+      color: #FEFCFB;
+    }
+  }
+  &.ultradark {
+    background-color: #001F45;
 
     a {
       color: #FEFCFB;
---
+++ mvc/src/views/Pregame.vue
@@ -0,0 +1,62 @@
+<template>
+    <div class="pregame">
+        <div class="category">
+            <h2>Catégorie</h2>
+            <select>
+                <option value="">--Please choose an option--</option>
+            </select>
+        </div>
+        <div class="difficulty">
+            <h2>Difficulté</h2>
+            <select>
+                <option value="easy">Facile</option>
+                <option value="medium">Intermédiaire</option>
+                <option value="hard">Difficile</option>
+            </select>
+        </div>
+        <ButtonCustom link="/qcm" name="C'est parti !" color="dark" size="big"/>
+    </div>
+</template>
+
+<script>
+import ButtonCustom from "@/components/ButtonCustom"
+
+export default {
+    name: "Pregame",
+    components: {
+        ButtonCustom
+    }
+}
+</script>
+
+<style lang="scss" scoped>
+.pregame {
+    height: 100vh;
+    display: flex;
+    flex-direction: column;
+    justify-content: center;
+    align-items: center;
+
+    .category, .difficulty {
+        width: 800px;
+        margin-bottom: 30px;
+
+        h2 {
+            font-size: 24px;
+            color: #0A1128;
+            margin-left: 20px;
+            margin-bottom: 10px;
+        }
+
+        select {
+            height: 100px;
+            width: 100%;
+            border-radius: 10px;
+            font-size: 36px;
+            color: #FEFCFB;
+            background-color: #034078;
+            outline: none;
+        }
+    }
+}
+</style>
