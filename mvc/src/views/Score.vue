e5f6cb35ace46c6cdfc52684edb0b8a12bb14f87
Tais-Juhel <45936782+Tais-Juhel@users.noreply.github.com>
2021-12-02 01:50:55 +0100

    Update Score.vue

--- mvc/src/views/Score.vue
+++ mvc/src/views/Score.vue
@@ -8,10 +8,13 @@
                 <p>pts</p>
             </div>
         </div>
+        <ButtonCustom name="Retour au menu" size="big" color="ultradark"/>
     </div>
 </template>
 
 <script>
+import ButtonCustom from '@/components/ButtonCustom'
+
 export default {
     name: "Score",
     data() {
@@ -19,5 +22,64 @@ export default {
             score: 8000
         }
     },
+    components: {
+        ButtonCustom
+    }
 }
 </script>
+
+<style lang="scss" scoped>
+.score {
+    display: flex;
+    justify-content: center;
+    align-items: center;
+    flex-direction: column;
+    height: 100vh;
+
+    .score-card {
+        position: relative;
+        display: flex;
+        flex-direction: column;
+        justify-content: center;
+        align-items: center;
+        height: 300px;
+        width: 900px;
+        background-color: #034078;
+        border-radius: 20px;
+        margin: 50px 0;
+
+        &::after {
+            content: "";
+            position: absolute;
+            z-index: -1;
+            top: 15px;
+            left: 15px;
+            width: 900px;
+            height: 300px;
+            border-radius: 20px;
+            background-color: #1282A2;
+        }
+
+        h2 {
+            font-size: 42px;
+            color: #FEFCFB;
+        }
+
+        .score-div {
+            display: flex;
+            align-items: flex-end;
+            color: #FEFCFB;
+
+            p {
+                font-size: 28px;
+            }
+
+            span {
+                font-size: 90px;
+                margin: 0 20px;
+                height: 100px;
+            }
+        }
+    }
+}
+</style>
